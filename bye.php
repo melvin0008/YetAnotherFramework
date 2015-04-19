a9071d0d25f1ad78c6e7b77dd4793f42450e5801
Melvin <melvin0008@gmail.com>
2015-04-20 01:57:09 +0530

    changed init.php to provide abstraction

--- bye.php
+++ bye.php
@@ -1,10 +1,6 @@
-<?
-require_once __DIR__.'/autoload.php';
+<?php
+
+require_once __DIR__.'/init.php';
  
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
- 
-$request = Request::createFromGlobals();
- 
-$response = new Response('Goodbye!');
+$response->setContent('Goodbye!');
 $response->send();
--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
 
-require_once __DIR__.'/autoload.php';
+require_once __DIR__.'/init.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-
-$request = Request::createFromGlobals();
-$input = $request->get('name', 'World');
-
-$response = new Response();
- 
-$response->setContent('Hello world!');
+$input = $request->get('name', 'World'); 
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 $response->setStatusCode(200);
 $response->headers->set('Content-Type', 'text/html');
