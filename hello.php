a93e8e4089da4b0c09f1d2a91cf6b139a56dddd7
Melvin <melvin0008@gmail.com>
2015-04-20 02:40:42 +0530

    changed hello to reflect changes in front.php

--- hello.php
+++ hello.php
@@ -1,4 +1,3 @@
-<?php
-
-$input = $request->get('name', 'World');
-$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+<?php $name = $request->get('name', 'World') ?>
+ 
+Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
